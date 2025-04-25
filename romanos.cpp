7b8c433a731cf48885e3b88868af822fe7415268
bdebatata <bdebatatadoce@gmail.com>
2025-04-24 22:23:21 -0300

    Implementação para o primeiro teste proposto

--- romanos.cpp
+++ romanos.cpp
@@ -1,6 +1,23 @@
 #include "romanos.hpp"
+#include <bits/stdc++.h>
+using namespace std;
 int romanos_para_decimal(char const * num_romano)
 {
+  map<string, int> dict_numeros = numeros_romanos();
+  if (strlen(num_romano) == 1){
+    return dict_numeros[num_romano];
+  }
   return 0; 
+}
+map<string, int> numeros_romanos(){
+  map<string, int> numeros_romanos;
+  numeros_romanos["I"] = 1;
+  numeros_romanos["V"] = 5;
+  numeros_romanos["X"] = 10;
+  numeros_romanos["L"] = 50;
+  numeros_romanos["C"] = 100;
+  numeros_romanos["D"] = 500;
+  numeros_romanos["M"] = 1000;
+  return numeros_romanos;
 }
  
--- romanos.hpp
+++ romanos.hpp
@@ -1,2 +1,3 @@
+#include <bits/stdc++.h>
 int romanos_para_decimal(char const * num_romano);
- 
+std::map<std::string, int> numeros_romanos();
