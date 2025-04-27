0149061e772cc659003d411e560f504182b7882d
bdebatata <bdebatatadoce@gmail.com>
2025-04-27 17:40:12 -0300

    Passando no quarto teste

--- romanos.cpp
+++ romanos.cpp
@@ -1,14 +1,16 @@
+// Copyright 2025 Bruno Henrique Duarte
 #include "romanos.hpp"
 #include <bits/stdc++.h>
-using namespace std;
-int romanos_para_decimal(char const * num_romano)
-{
-  map<string, int> dict_numeros = numeros_romanos();
+#include <string>
+#include <map>
+int romanos_para_decimal(char const * num_romano) {
+  std::map<std::string, int> dict_numeros = numeros_romanos();
   // Para os dois primeiros caso de teste se avalia a string de somente um char;
-  if (strlen(num_romano) == 1){
-    // Se o algarismo passado faz parte do mapeamento feito, retorna o valor. Senão, o algarismo não existe
+  if (strlen(num_romano) == 1) {
+    // Se o algarismo passado faz parte do mapeamento feito, retorna o valor.
+    // Senão, o algarismo não existe
     auto it = dict_numeros.find(num_romano);
-    if (it != dict_numeros.end()){
+    if (it != dict_numeros.end()) {
       return dict_numeros[num_romano];
     } else {
       return -1;
@@ -17,27 +19,26 @@ int romanos_para_decimal(char const * num_romano)
     // Para o terceiro e quarto caso de teste
     int contador = 0;
     // Se itera sobre os char da string passada e convertendo para decimal.
-    // Se o valor em num_romano[i] for menor que num_romano[i+1] então se soma ao contador num_romano[i+1]-num_romano[i];
-    for(int i=0; i<strlen(num_romano); i++){
-      string atual(1, num_romano[i]);
-      string prox(1, num_romano[i+1]);
+    // Se o valor em num_romano[i] for menor que num_romano[i+1]
+    // Então se soma ao contador num_romano[i+1]-num_romano[i];
+    for (int i = 0; i < strlen(num_romano); i++) {
+      std::string atual(1, num_romano[i]);
+      std::string prox(1, num_romano[i+1]);
       int valor_decimal_atual = dict_numeros[atual];
       int valor_decimal_prox = dict_numeros[prox];
-      if(valor_decimal_prox>valor_decimal_atual){
-        contador+=(valor_decimal_prox-valor_decimal_atual); 
-        i+=1; 
+      if (valor_decimal_prox > valor_decimal_atual) {
+        contador += (valor_decimal_prox-valor_decimal_atual);
+        i += 1;
       } else {
         contador+=valor_decimal_atual;
       }
-      
     }
     return contador;
   }
-  return 0; 
 }
-// Função para retornar o mapeamento dos algarismos únicos romanos em valores decimais.
-map<string, int> numeros_romanos(){
-  map<string, int> numeros_romanos;
+// Função para retornar o mapeamento dos algarismos romanos em valores decimais.
+std::map< std::string, int > numeros_romanos() {
+  std::map<std::string, int> numeros_romanos;
   numeros_romanos["I"] = 1;
   numeros_romanos["V"] = 5;
   numeros_romanos["X"] = 10;
@@ -47,4 +48,3 @@ map<string, int> numeros_romanos(){
   numeros_romanos["M"] = 1000;
   return numeros_romanos;
 }
- 
